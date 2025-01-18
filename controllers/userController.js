ce5d30751d3d0f1dc092f6c2d30c3935b0191c6b
manoxx <mbpi.obpl@gmail.com>
2025-01-19 01:06:27 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const cloudinary = require("cloudinary").v2;
+const multer = require('multer');
+
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,13 +9,22 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
+
+
 const uploadUser = async (req, res)=>{
 
     try{
         const {name, socialHandle} = req.body;
         const imageUrls = await Promise.all(req.files.map(async(file)=>{
-            const result = await cloudinary.uploader.upload(file.path);
-            return result.secure_url;
+            const result = await cloudinary.uploader.upload_stream({
+                resource_type: "auto"
+            }, (error, result)=>{
+                if(error) throw new Error(error.message);
+                return result.secure_url;
+            });
+            result.end(file.buffer);
         }));
 
         const newUser = new User({name, socialHandle, images: imageUrls});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,8 @@ const {uploadUser, getSubmissions } = require("../controllers/userController");
 const verifyAdmin = require("../middleware/authMiddleware")
 
 const router = express.Router();
-const upload = multer({dest: "uploads/"});
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
 
 router.post("/upload", upload.array("images", 5), uploadUser);
 router.get("/submissions", verifyAdmin, getSubmissions);
